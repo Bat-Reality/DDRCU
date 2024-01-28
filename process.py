84a4f446a80d457f884d83d94bb56752cd404d77
Bat-Reality <56718188+Bat-Reality@users.noreply.github.com>
2024-01-28 13:52:43 +0800

    Update process.py

--- process.py
+++ process.py
@@ -50,10 +50,6 @@ strategies = json.load(open('./_reformat/strategy.json'))
 strategies = [e[1:-1] for e in strategies]
 strat2id = {strat: i for i, strat in enumerate(strategies)}
 print(args.add_persona)
-# if args.add_persona:
-#     original = json.load(open('./_reformat/DPRConv_5.json'))
-# else:
-#     original = json.load(open('./_reformat/ESConv.json'))
 
 
 def get_commonsense(comet, item):
@@ -65,61 +61,6 @@ def get_commonsense(comet, item):
         cs_list.append(cs_res)
 
     return cs_list
-
-
-# def comet_data(d):
-#     emotion = d['emotion_type']
-#     problem = d["problem_type"]
-#     situation = d['situation']
-#     persona = d['persona']
-#     persona_list = d['persona_list']
-#
-#     d = d['dialog']
-#     dial = []
-#     pre_text = ""
-#
-#     for uttr in d:
-#         text = _norm(uttr['content'])
-#         role = uttr['speaker']
-#
-#         if role == 'seeker':
-#             dial.append({
-#                 'text': text,
-#                 'speaker': 'usr',
-#             })
-#         else:
-#             if len(uttr) > 3:
-#                 post = _norm(pre_text)
-#                 item = process_sent(post)
-#                 cs_list = get_commonsense(comet, item)
-#
-#                 dial.append({
-#                     'text': text,
-#                     'speaker': 'sys',
-#                     'strategy': uttr['annotation']['strategy'],
-#                     'dpr': uttr['dpr'],
-#                     'comet': cs_list
-#                 })
-#             else:
-#                 dial.append({
-#                     'text': text,
-#                     'speaker': 'sys',
-#                     'strategy': uttr['annotation']['strategy'],
-#                 })
-#         pre_text = uttr['content']
-#
-#     res = {
-#         'emotion_type': emotion,
-#         'problem_type': problem,
-#         'persona': persona,
-#         'persona_list': persona_list,
-#         'situation': situation,
-#         # 'init_intensity': init_intensity,
-#         # 'final_intensity': final_intensity,
-#         'dialog': dial,
-#     }
-#
-#     return res
 
 
 def comet_data(d):
@@ -178,21 +119,12 @@ def comet_data(d):
         'persona': persona,
         'persona_list': persona_list,
         'situation': situation,
-        # 'init_intensity': init_intensity,
-        # 'final_intensity': final_intensity,
         'dialog': dial,
     }
 
     return res
 
 
-"""
-1: DPRConv_1
-2: DPRConv_2
-3: DPRConv_2 + persona (🐶)
-4: DPRConv_1 + persona
-5: DPRConv_5 + persona
-"""
 comet = Comet("src/utils/data/ED/Comet", device)
 # comet = Comet("src/utils/data/ED/Comet_GPT2", device)
 data = []
@@ -206,37 +138,8 @@ for dataset in datasets:
     with open('./DATA/6_' + dataset, 'w') as f:
         for e in data:
             f.write(json.dumps(e) + '\n')
-# with mp.Pool(processes=mp.cpu_count()) as pool:
-#     for e in pool.imap(comet_data, tqdm.tqdm(original, total=len(original))):
-#         data.append(e)
-# for e in map(comet_data, tqdm.tqdm(original, total=len(original))):
-#     data.append(e)
 
 emotions = Counter([e['emotion_type'] for e in data])
 problems = Counter([e['problem_type'] for e in data])
 print('emotion', emotions)
 print('problem', problems)
-
-# random.shuffle(data)
-# dev_size = int(0.1 * len(data))
-# test_size = int(0.1 * len(data))
-# valid = data[:dev_size] + data[dev_size + test_size: dev_size + dev_size + test_size]
-# test = data[dev_size: dev_size + test_size]
-# train = data[dev_size + dev_size + test_size:]
-#
-# print('train', len(train))
-# with open('./train.txt', 'w') as f:
-#     for e in train:
-#         f.write(json.dumps(e) + '\n')
-# with open('./sample.json', 'w') as f:
-#     json.dump(train[:10], f, ensure_ascii=False, indent=2)
-#
-# print('valid', len(valid))
-# with open('./valid.txt', 'w') as f:
-#     for e in valid:
-#         f.write(json.dumps(e) + '\n')
-#
-# print('test', len(test))
-# with open('./test.txt', 'w') as f:
-#     for e in test:
-#         f.write(json.dumps(e) + '\n')
