a501c66d76012016194e11641573b63ceb0d2e40
Bat-Reality <56718188+Bat-Reality@users.noreply.github.com>
2024-01-28 13:51:14 +0800

    Update strat_blenderbot_small.py

--- models/strat_blenderbot_small.py
+++ models/strat_blenderbot_small.py
@@ -221,17 +221,11 @@ class Model(BaseModel, BlenderbotSmallForConditionalGeneration):
         self.context_norm_2 = nn.LayerNorm(self.model.config.d_model, elementwise_affine=True)  # 512
         self.comet_norm = nn.LayerNorm(self.model.config.d_model, elementwise_affine=True)  # 512
         self.norm = nn.LayerNorm(self.model.config.d_model, elementwise_affine=True)  # 512
-        # self.persona_context_w = nn.Parameter(torch.tensor([0.5, 0.5]))
-        # self.persona_context_w = nn.Parameter(torch.tensor([1 / 3, 1 / 3, 1 / 3]))
-        # self.persona_context_w = nn.Parameter(torch.tensor([1 / 4, 1 / 4, 1 / 4, 1 / 4]))
         self.persona_context_w = nn.Parameter(torch.tensor([1 / 5, 1 / 5, 1 / 5, 1 / 5, 1 / 5]))
         self.my_past = None
         self.my_encoder_outputs = None
         self.generation_strategy = None
-        # self.strategy_alpha = [0.75, 0.75, 0.75, 0.75, 0.75, 0.75, 0.75, 0.75]
-        # self.strategy_alpha = [0, 0.75, 0, 0, 0.75, 0.75, 0.75, 0.375]
         self.train_alpha = False
-        # self.strategy_alpha = nn.Parameter(torch.tensor([0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 0.5]))
         self.strategy_alpha = [0, 0, 0, 0, 0, 0, 0, 0]
         # "[Question]","[Restatement or Paraphrasing]",
         # "[Reflection of feelings]","[Self-disclosure]",
@@ -304,7 +298,6 @@ class Model(BaseModel, BlenderbotSmallForConditionalGeneration):
             )
 
         if not persona_change_attention or persona_input_ids is None:
-            # print(input_ids, attention_mask, decoder_input_ids, encoder_outputs, (past_key_values is None))
             outputs = self.model(
                 input_ids,
                 attention_mask=attention_mask,
@@ -346,21 +339,12 @@ class Model(BaseModel, BlenderbotSmallForConditionalGeneration):
                 output_hidden_states=output_hidden_states,
                 return_dict=return_dict,
             )
-            # print(encoder_outputs[0].size())
-            # tmp = [torch.matmul(torch.softmax(torch.matmul(i, j.t()), dim=-1), j) for i, j in zip(encoder_outputs[0], persona_encoder_outputs[0])]
-            # print(len(tmp), tmp[0].size())
-            # encoder_outputs.last_hidden_state = torch.stack(
-            #     [torch.matmul(torch.softmax(torch.matmul(i, j.t()), dim=-1), j) for i, j in
-            #      zip(encoder_outputs[0], persona_encoder_outputs[0])])
             context = torch.stack([torch.matmul(torch.softmax(torch.matmul(j, i.t()), dim=-1), i) for i, j in
                                    zip(encoder_outputs[0], persona_encoder_outputs[0])])
             persona = torch.stack([torch.matmul(torch.softmax(torch.matmul(i, j.t()), dim=-1), j) for i, j in
                                    zip(encoder_outputs[0], persona_encoder_outputs[0])])
             context_persona = self.persona_norm(context+persona_encoder_outputs.last_hidden_state)
             persona = self.context_norm_1(encoder_outputs.last_hidden_state+persona)
-            # print(context_persona.shape)    # torch.Size([8, 512, 512])
-            # print(persona.shape)            # torch.Size([8, 512, 512])
-            # print(encoder_outputs.last_hidden_state.shape)  # torch.Size([8, 512, 512])
 
             # Comet
             comet_encoder_outputs = self.model.encoder(
@@ -373,17 +357,10 @@ class Model(BaseModel, BlenderbotSmallForConditionalGeneration):
                 return_dict=return_dict,
             )
             comet_output = self.cog_encoder(comet_encoder_outputs[0], comet_attention_mask.unsqueeze(1))
-            # print(comet_output.shape)   # torch.Size([4, 512, 512])
             cls_tokens = comet_output[:, 0].unsqueeze(1)
-            # cls_tokens = comet_encoder_outputs[0][:, 0].unsqueeze(1)
-            # print(cls_tokens.shape)     # torch.Size([4, 1, 512])
             dim = [-1, encoder_outputs[0].shape[1], -1]
             cog_concat = torch.cat([encoder_outputs[0], cls_tokens.expand(dim)], dim=-1)
-            # cog_concat = torch.cat([encoder_outputs[0], comet_output], dim=-1)
-            # print(cog_concat.shape)     # torch.Size([4, 512, 1024])
-            # cog_concat_enc = self.cog_ref_encoder(cog_concat, comet_attention_mask.unsqueeze(1))
             cog_concat_enc = self.cog_ref_encoder(cog_concat, attention_mask.unsqueeze(1))
-            # print(cog_concat_enc.shape)     # torch.Size([4, 512, 512])
             cog_contrib = nn.Sigmoid()(cog_concat_enc)
             cog_ref_ctx = cog_contrib * cog_concat_enc
             cog_concat_enc = self.cog_lin(cog_ref_ctx)
@@ -394,12 +371,6 @@ class Model(BaseModel, BlenderbotSmallForConditionalGeneration):
                                  zip(encoder_outputs[0], cog_concat_enc)])
             context_comet = self.comet_norm(context + cog_concat_enc)
             comet = self.context_norm_2(encoder_outputs.last_hidden_state + comet)
-            # context = torch.stack([torch.matmul(torch.softmax(torch.matmul(j, i.t()), dim=-1), i) for i, j in
-            #                        zip(encoder_outputs[0], comet_encoder_outputs[0])])  # torch.Size([4, 512, 512])
-            # comet = torch.stack([torch.matmul(torch.softmax(torch.matmul(i, j.t()), dim=-1), j) for i, j in
-            #                      zip(encoder_outputs[0], comet_encoder_outputs[0])])
-            # context_comet = self.comet_norm(context + comet_encoder_outputs.last_hidden_state)
-            # comet = self.context_norm_2(encoder_outputs.last_hidden_state + comet)
 
             # 归一化权重
             w1 = torch.exp(self.persona_context_w[0]) / torch.sum(torch.exp(self.persona_context_w))
@@ -407,25 +378,9 @@ class Model(BaseModel, BlenderbotSmallForConditionalGeneration):
             w3 = torch.exp(self.persona_context_w[2]) / torch.sum(torch.exp(self.persona_context_w))
             w4 = torch.exp(self.persona_context_w[3]) / torch.sum(torch.exp(self.persona_context_w))
             w5 = torch.exp(self.persona_context_w[4]) / torch.sum(torch.exp(self.persona_context_w))
-            # encoder_outputs.last_hidden_state = w1 * encoder_outputs.last_hidden_state + w2 * context_persona + w3 * persona
-            # encoder_outputs.last_hidden_state = w1 * encoder_outputs.last_hidden_state + w2 * context_persona + w3 * context_comet
-            # encoder_outputs.last_hidden_state = w1 * encoder_outputs.last_hidden_state + w2 * context_comet + w3 * comet
-            # encoder_outputs.last_hidden_state = w1 * encoder_outputs.last_hidden_state + w2 * persona_encoder_outputs.last_hidden_state + w3 * comet_encoder_outputs.last_hidden_state
             encoder_outputs.last_hidden_state = w1 * encoder_outputs.last_hidden_state + w2 * context_persona + w3 * persona + w4 * context_comet + w5 * comet
-            # encoder_outputs.last_hidden_state = w1 * encoder_outputs.last_hidden_state + w2 * context_persona + w3 * persona + w4 * cls_tokens.expand(dim)
-            # encoder_outputs.last_hidden_state = w1 * encoder_outputs.last_hidden_state + w2 * context_persona + w3 * persona + w4 * cog_concat_enc
-            # encoder_outputs.last_hidden_state = self.norm(w1 * encoder_outputs.last_hidden_state + w2 * context_persona + w3 * persona + w4 * context_comet + w5 * comet)
             encoder_outputs.last_hidden_state = self.norm(w1 * encoder_outputs.last_hidden_state + w2 * context_persona + w3 * persona + w4 * context_comet + w5 * comet)
 
-            # encoder_outputs.last_hidden_state = w1 * context + w2 * persona
-            # encoder_outputs.last_hidden_state = self.persona_context_w[0]*context + self.persona_context_w[1]*persona
-            # encoder_outputs.last_hidden_state = self.persona_context_w[0]*encoder_outputs.last_hidden_state + self.persona_context_w[1]*persona
-
-            # encoder_outputs.last_hidden_state = self.persona_context_w[0] * persona + \
-            #                                     self.persona_context_w[1] * encoder_outputs.last_hidden_state + \
-            #                                     self.persona_context_w[2] * context
-            # print(self.persona_context_w)
-            # decoder outputs consists of (dec_features, past_key_value, dec_hidden, dec_attn)
             decoder_outputs = self.model.decoder(
                 input_ids=decoder_input_ids,
                 attention_mask=decoder_attention_mask,
@@ -456,7 +411,6 @@ class Model(BaseModel, BlenderbotSmallForConditionalGeneration):
                 )
 
         lm_logits = self.lm_head(outputs[0]) + self.final_logits_bias
-        # TODO: 考虑搞成两部分loss
 
         if self.train_alpha and labels is not None:
             my_logits = self.lm_head(my_outputs[0]) + self.final_logits_bias
@@ -478,29 +432,13 @@ class Model(BaseModel, BlenderbotSmallForConditionalGeneration):
         masked_lm_loss = None
         if labels is not None:
             # 两部分loss相加
-            # logits_strategy = lm_logits[:, :1, -8:].contiguous()
-            # lm_logits = lm_logits[:, 1:, :len(self.toker) - 8].contiguous()
-            # strategy = labels[:, :1].contiguous()
-            # strategy = strategy - (len(self.toker)-8) * torch.ones_like(strategy, device=strategy.get_device())
-            # labels = labels[:, 1:].contiguous()
-            # print(logits_strategy.size(), strategy.size(), lm_logits.size(), labels.size())
-
-            # print(lm_logits.size(), labels.size())
             loss = F.cross_entropy(lm_logits.view(-1, lm_logits.size(-1)), labels.view(-1), reduction='none')
             loss = loss.view(labels.size(0), labels.size(1))
             label_size = torch.sum(labels.ne(-100), dim=1).type_as(loss)
-            # print("not -100: ", torch.sum(labels.ne(-100), dim=1), "equal -100: ", torch.sum(torch.logical_not(labels.ne(-100)), dim=1))
             masked_lm_loss = torch.sum(loss) / torch.sum(label_size)
             ppl_value = torch.exp(torch.mean(torch.sum(loss, dim=1).float() / label_size.float()))
-            # print(strategy, logits_strategy)
-            # strategy_loss = F.cross_entropy(logits_strategy.view(-1, logits_strategy.size(-1)), strategy.view(-1), reduction='none')
-            # strategy_loss = strategy_loss.view(strategy.size(0), strategy.size(1))
-            # print(masked_lm_loss, strategy_loss / 4)
-            # masked_lm_loss += torch.sum(strategy_loss) / strategy.size(0)
 
         if not self.training and not validation:  # inference
-            # print(input_ids, attention_mask, self.my_past, self.my_encoder_outputs)
-            # print(self.strategy_alpha)
             my_outputs = self.model(
                 input_ids,
                 attention_mask=attention_mask,
@@ -512,10 +450,7 @@ class Model(BaseModel, BlenderbotSmallForConditionalGeneration):
             )
             self.my_past = my_outputs.past_key_values
             my_logits = self.lm_head(my_outputs[0]) + self.final_logits_bias
-            # alpha = 0.75
-            # alpha = 0
             alpha_l = []
-            # print(lm_logits.get_device())
             if lm_logits.get_device() == -1:
                 device = 'cpu'
             else:
@@ -526,12 +461,7 @@ class Model(BaseModel, BlenderbotSmallForConditionalGeneration):
                 tmp_alpha = tmp_alpha * torch.ones(lm_size[1], lm_size[2], device=device)
                 alpha_l.append(tmp_alpha)
             alpha_l = torch.stack(alpha_l)
-            # print(alpha_l.size(), alpha_l)
-            # assert 0
-            # print(lm_logits.get_device())
             lm_logits = (torch.ones_like(lm_logits, device=device)+alpha_l)*lm_logits - alpha_l*my_logits
-            # lm_logits = my_top_k_top_p_filtering(lm_logits, top_k=10)
-            # lm_logits = (1+alpha)*lm_logits - alpha*my_logits
 
             if not return_dict:
                 output = (lm_logits,) + outputs[1:]
@@ -624,12 +554,6 @@ class Model(BaseModel, BlenderbotSmallForConditionalGeneration):
                 attention_mask=persona_attention_mask,
                 return_dict=return_dict,
             )
-            # encoder_outputs.last_hidden_state = torch.stack(
-            #     [torch.matmul(torch.softmax(torch.matmul(i, j.t()), dim=-1), j) for i, j in
-            #      zip(encoder_outputs[0], persona_encoder_outputs[0])])
-            # encoder_outputs.last_hidden_state = torch.stack(
-            #     [torch.matmul(torch.softmax(torch.matmul(j, i.t()), dim=-1), i) for i, j in
-            #      zip(encoder_outputs[0], persona_encoder_outputs[0])])
             context = torch.stack([torch.matmul(torch.softmax(torch.matmul(j, i.t()), dim=-1), i) for i, j in
                                    zip(encoder_outputs[0], persona_encoder_outputs[0])])
             persona = torch.stack([torch.matmul(torch.softmax(torch.matmul(i, j.t()), dim=-1), j) for i, j in
@@ -644,17 +568,10 @@ class Model(BaseModel, BlenderbotSmallForConditionalGeneration):
                 return_dict=return_dict,
             )
             comet_output = self.cog_encoder(comet_encoder_outputs[0], comet_attention_mask.unsqueeze(1))
-            # print(comet_output.shape)   # torch.Size([4, 512, 512])
             cls_tokens = comet_output[:, 0].unsqueeze(1)
-            # cls_tokens = comet_encoder_outputs[0][:, 0].unsqueeze(1)
-            # print(cls_tokens.shape)     # torch.Size([4, 1, 512])
             dim = [-1, encoder_outputs[0].shape[1], -1]
             cog_concat = torch.cat([encoder_outputs[0], cls_tokens.expand(dim)], dim=-1)
-            # cog_concat = torch.cat([encoder_outputs[0], comet_output], dim=-1)
-            # print(cog_concat.shape)     # torch.Size([4, 512, 1024])
-            # cog_concat_enc = self.cog_ref_encoder(cog_concat, comet_attention_mask.unsqueeze(1))
             cog_concat_enc = self.cog_ref_encoder(cog_concat, attention_mask.unsqueeze(1))
-            # print(cog_concat_enc.shape)     # torch.Size([4, 512, 512])
             cog_contrib = nn.Sigmoid()(cog_concat_enc)
             cog_ref_ctx = cog_contrib * cog_concat_enc
             cog_concat_enc = self.cog_lin(cog_ref_ctx)
@@ -672,22 +589,7 @@ class Model(BaseModel, BlenderbotSmallForConditionalGeneration):
             w3 = torch.exp(self.persona_context_w[2]) / torch.sum(torch.exp(self.persona_context_w))
             w4 = torch.exp(self.persona_context_w[3]) / torch.sum(torch.exp(self.persona_context_w))
             w5 = torch.exp(self.persona_context_w[4]) / torch.sum(torch.exp(self.persona_context_w))
-            # encoder_outputs.last_hidden_state = w1 * encoder_outputs.last_hidden_state + w2 * context_persona + w3 * persona
-            # encoder_outputs.last_hidden_state = w1 * encoder_outputs.last_hidden_state + w2 * context_persona + w3 * context_comet
-            # encoder_outputs.last_hidden_state = w1 * encoder_outputs.last_hidden_state + w2 * context_comet + w3 * comet
-            # encoder_outputs.last_hidden_state = w1 * encoder_outputs.last_hidden_state + w2 * persona_encoder_outputs.last_hidden_state + w3 * comet_encoder_outputs.last_hidden_state
-            # encoder_outputs.last_hidden_state = w1 * encoder_outputs.last_hidden_state + w2 * context_persona + w3 * persona + w4 * context_comet + w5 * comet
-            # encoder_outputs.last_hidden_state = w1 * encoder_outputs.last_hidden_state + w2 * context_persona + w3 * persona + w4 * cls_tokens.expand(dim)
-            # encoder_outputs.last_hidden_state = w1 * encoder_outputs.last_hidden_state + w2 * context_persona + w3 * persona + w4 * cog_concat_enc
             encoder_outputs.last_hidden_state = self.norm(w1 * encoder_outputs.last_hidden_state + w2 * context_persona + w3 * persona + w4 * context_comet + w5 * comet)
-
-            # encoder_outputs.last_hidden_state = self.persona_context_norm(persona + context)
-            # encoder_outputs.last_hidden_state = self.persona_context_w[0]*encoder_outputs.last_hidden_state + self.persona_context_w[1]*persona
-            # encoder_outputs.last_hidden_state = self.persona_context_w[0] * persona + self.persona_context_w[1] * encoder_outputs.last_hidden_state
-            # encoder_outputs.last_hidden_state = self.persona_context_w[0] * persona + \
-            #                                     self.persona_context_w[1] * encoder_outputs.last_hidden_state + \
-            #                                     self.persona_context_w[2] * context
-            # print(self.persona_context_w)
 
         decoder_outputs = self.model.decoder(
             input_ids=decoder_input_ids,
@@ -706,15 +608,11 @@ class Model(BaseModel, BlenderbotSmallForConditionalGeneration):
         )
         my_logits = self.lm_head(my_outputs[0]) + self.final_logits_bias
         alpha = 0.075
-        # alpha = 0
         lm_logits = (1+alpha)*lm_logits - alpha*my_logits
         self.predict_strategy(lm_logits, encoded_info)
         self.generation_strategy = encoded_info['pred_strat_id']
         decoder_input_ids = torch.cat(
             [decoder_input_ids, encoded_info['pred_strat_id'][..., None] + len(self.toker) - 8], dim=-1)
-        # print(encoded_info)
-        # print(decoder_input_ids)
-        # print(self.toker.batch_decode(decoder_input_ids))
         assert 'max_length' in kwargs
         kwargs['max_length'] = kwargs['max_length'] + decoder_input_ids.size(1)
         kwargs['use_cache'] = True
@@ -729,7 +627,5 @@ class Model(BaseModel, BlenderbotSmallForConditionalGeneration):
             decoder_input_ids=decoder_input_ids,
             **kwargs
         )
-        # print(encoded_info)
         encoded_info['persona'] = persona_input_ids
-        # print(persona_input_ids)
         return encoded_info, generations[:, decoder_input_ids.size(1):]
